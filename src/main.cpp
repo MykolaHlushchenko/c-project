6b1ed63ef8ef1954d068e047ede5b043ac80c4c7
myhlu <myhlu@smile.fr>
2024-11-25 15:12:54 +0200

    [LED] change readme file

--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,21 @@ const int ledPin = 2;
 
 void setup() {
   // put your setup code here, to run once:
-  //int result = myFunction(2, 3);
   pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
   // increase the LED brightness
-  for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){   
+  for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++) {   
     // changing the LED brightness with PWM
     analogWrite(ledPin, dutyCycle);
-    delay(200);
+    delay(20);
   }
 
   // decrease the LED brightness
-  for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--){
+  for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--) {
     // changing the LED brightness with PWM
     analogWrite(ledPin, dutyCycle);
-    delay(200);
+    delay(20);
   }
 }
