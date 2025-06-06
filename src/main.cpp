fa62bf9b816189bae48b57b181ecd2e11db35491
Mykola Hlushchenko <myhlu@smile.fr>
2025-06-06 15:04:40 +0300

    [TEMPERATURE] Add random temperature disaplay

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,17 @@
 #include <Arduino.h>
 
-const int ledPin = 2;
-
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(ledPin, OUTPUT);
+  Serial.begin(115200);
+  delay(1000);
+
+  Serial.println("Start monitoring programm");
 }
 
 void loop() {
-  // increase the LED brightness
-  for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++) {   
-    // changing the LED brightness with PWM
-    analogWrite(ledPin, dutyCycle);
-    delay(20);
-  }
+  float temp = random(200, 300) / 10.0;
+  Serial.print("Temperature ");
+  Serial.print(temp);
+  Serial.println("C");
 
-  // decrease the LED brightness
-  for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--) {
-    // changing the LED brightness with PWM
-    analogWrite(ledPin, dutyCycle);
-    delay(20);
-  }
+  delay(2000);
 }
